7ce0ff04bd1cb95330ecc8b34101c05f14873cb6
liaoxiaoyue <179423082@qq.com>
2018-04-28 16:30:13 +0800

    创建项目

---
+++ DYZB/DYZB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYZB
+//
+//  Created by 红喇叭 on 2018/4/28.
+//  Copyright © 2018年 红喇叭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DYZB/DYZB/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DYZB
+//
+//  Created by 红喇叭 on 2018/4/28.
+//  Copyright © 2018年 红喇叭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
